9b973003732ba2b666ae7b5a358b3e7aaea242ea
joanaizabeldossantos <110937614+joanaizabeldossantos@users.noreply.github.com>
2022-10-05 16:59:24 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -3,9 +3,7 @@
 // console.log("Eu estou no console.");
 let tabuada = 5;
 function alo(){
-
-    let tabuada = document.getElementById("entrada").value;
-
+    let tabuada = document.getElementById("entrada").value
     document.write("<h1>Tabuada do " + tabuada + "</h1>");
     document.write(tabuada + " x 1 = " + (tabuada*1)+"<br>");
     document.write(tabuada + " x 2 = " + (tabuada*2)+"<br>");
@@ -27,7 +25,7 @@ function escreva(){
         document.write("<br>");
     }
 }
-
+ 
 function quadrado(){
     for(var i = 2; i < 11; i++){
         document.write("O quadrado de " + i + " é " + (i*i)+"<br>");
@@ -38,5 +36,7 @@ function total(){
     let val = document.getElementById("valor").value;
     let ju = document.getElementById("juros").value;
     let resultado = (val * ((ju/100)+1));
-    document.write("O total é de: " + resultado);
+    document.write("o total é de: " + resultado);
 }
+
+
